c7b758e3521172e877aa1b965a46918214ec2550
shiraitoshiyuki <shirai.toshiyuki.1021@gmail.com>
2021-07-05 10:50:14 +0900

    test param

---
+++ pages/items/_item_id.vue
@@ -0,0 +1,17 @@
+<template>
+<div>
+  {{this.$route.params.item_id}}
+  <v-btn color="primary">ボタン</v-btn>
+
+</div>
+</template>
+
+<script>
+export default {
+  name: "test"
+}
+</script>
+
+<style scoped>
+
+</style>
