f14e75bb3e379acc2eca5417287e9760254a7f3e
shiraitoshiyuki <shirai.toshiyuki.1021@gmail.com>
2021-07-05 19:01:50 +0900

    test store

--- pages/items/index.vue
+++ pages/items/index.vue
@@ -1,13 +1,21 @@
 <template>
 <div>
-  <v-btn color="primary">ボタン</v-btn>
+  <v-badge>{{$store.state.counter}}</v-badge>
+  <v-btn color="primary" @click="this.countUp">ボタン</v-btn>
 </div>
 </template>
 
 <script>
 export default {
-  name: "test"
+  name: "test",
+  methods:{
+    countUp(){
+      this.$store.commit('increment')
+    }
+  }
 }
+
+
 </script>
 
 <style scoped>
